ca013fa9100562c899d09f313c4016dabe2484f8
Michael <michael.egwali@andela.com>
2021-06-28 14:07:46 +0100

    add Container / routing component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-const App = () => {
-  return <div />;
-};
+import { BrowserRouter as Router } from 'react-router-dom';
+import Container from './components/Container';
 
+
+const App = () => (
+  <Router>
+    <Container />
+  </Router>
+);
 
 export default App;
