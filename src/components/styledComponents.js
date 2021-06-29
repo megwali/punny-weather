33e1a14318d2bbe29b4b324baf5cd91d169a3000
Michael <michael.egwali@andela.com>
2021-06-29 13:01:30 +0100

    Add Menu component

---
+++ src/components/styledComponents.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const MenuContainer = styled.nav`
+  border-bottom: 1px solid #ddd;
+  display: flex;
+  margin: 0 2rem 2rem;
+  padding-bottom: 1rem;
+  > a {
+    color: black;
+    margin-right: .5rem;
+    text-decoration: none;
+  }
+`;
