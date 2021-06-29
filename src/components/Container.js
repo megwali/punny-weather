80ceba898baf7b08ce891f71eda079b5f9a6e674
Michael <michael.egwali@andela.com>
2021-06-29 13:33:47 +0100

    Add Home and locate

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,18 @@
 import { Route, Switch } from 'react-router-dom';
-import { Dashboard, Details } from '../pages';
+import { Dashboard, Details, Home } from '../pages';
 
 
 const Container = () => (
   <Switch>
     <Route exact path="/">
+      <Home />
+    </Route>
+
+    <Route path="/locate">
+      <Dashboard />
+    </Route>
+
+    <Route path="/search">
       <Dashboard />
     </Route>
 
