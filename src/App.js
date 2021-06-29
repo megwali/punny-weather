e9ed3e511818a5e137960f4aa86de83a81ec5cbd
Michael <michael.egwali@andela.com>
2021-06-29 13:32:48 +0100

    Refactor App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Container from './components/Container';
+import Header from './components/Header';
+import Menu from './components/Menu';
 
 
 const App = () => (
   <Router>
+    <Header />
+    <Menu />
     <Container />
   </Router>
 );
--- src/specs/App.spec.js
+++ src/specs/App.spec.js
@@ -1,5 +1,9 @@
 import { create } from 'react-test-renderer';
-import App from './App';
+import App from '../App';
+
+jest.mock('../components/Container', () => 'Container');
+jest.mock('../components/Header', () => 'Header');
+jest.mock('../components/Menu', () => 'Menu');
 
 
 describe('App', () => {
