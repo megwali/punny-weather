58e6d6c3068daf8140d963d1baf7c433877d5e79
Michael <michael.egwali@andela.com>
2021-07-01 08:29:41 +0100

    update dashboard, unify styled components

--- src/components/styledComponents.js
+++ src/components/styledComponents.js
@@ -1,4 +1,65 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const CardList = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 4px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  margin: 1rem auto;
+  max-width: 80%;
+  min-height: 60px;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    max-width: 100%;
+  }
+`;
+
+export const CardText = styled.p`
+  margin: 0.25rem;
+`;
+
+export const CenteredTextContainer = styled.div`
+  margin: 0.5rem;
+  text-align: center;
+`;
+
+export const Error = styled(CenteredTextContainer)`
+  color: red;
+`;
+
+export const LinkedCard = styled(Link)`
+  border: 1px solid #555;
+  border-radius: 4px;
+  color: inherit;
+  line-height: 1.35;
+  margin: 0.75rem;
+  padding: 1rem 2rem 2rem;
+  text-decoration: none;
+  width: 33%;
+
+  > span {
+    text-decoration: underline;
+  }
+
+  @media (max-width: 768px) {
+    margin: 0 0 1.5rem;
+    padding: 1rem 0.5rem 2rem;
+    width: 100%;
+  }
+`;
+
+export const LinkedCardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
 
 export const MenuContainer = styled.nav`
   border-bottom: 1px solid #ddd;
@@ -9,5 +70,26 @@ export const MenuContainer = styled.nav`
     color: black;
     margin-right: .5rem;
     text-decoration: none;
+  }
+`;
+
+
+export const RowWrapper = styled.div`
+  align-items: center;
+  display: flex;
+`;
+
+export const ColWrapper = styled(RowWrapper)`
+  flex-direction: column;
+`;
+
+export const WeatherCard = styled(LinkedCard)`
+  background: white;
+  border: 1px solid lightgrey;
+  width: auto;
+
+  @media (max-width: 768px) {
+    margin: 0.75rem;
+    padding: 1rem 2rem 2rem;
   }
 `;
